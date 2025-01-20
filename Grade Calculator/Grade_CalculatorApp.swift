e42deb605e3053e289ee3addcd035a4b27403ec5
Daniel Li <danli@lcs.on.ca>
2025-01-20 09:01:43 -0500

    Initial Commit

---
+++ Grade Calculator/Grade_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grade_CalculatorApp.swift
+//  Grade Calculator
+//
+//  Created by 李泽宇 on 2025-01-20.
+//
+
+import SwiftUI
+
+@main
+struct Grade_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
